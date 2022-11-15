6ac5d8429ce25618eadadaea5e4b124a4452c09b
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2022-11-15 09:34:40 -0500

    Implemented fib using iteration

--- boss-game/main.swift
+++ boss-game/main.swift
@@ -9,6 +9,11 @@ import Foundation
 
 func fib(k:Int) -> Int
 {
+    if k == 0
+    {
+        return 0
+    }
+    
     if k == 1
     {
         return 1
@@ -17,16 +22,23 @@ func fib(k:Int) -> Int
     return fib(k: k-1) + fib(k: k-2)
 }
 
-//func fibi(k:Int) -> Int
-//{
-//    var fibArray :[Int] = []
-//    for 0..<k
-//    {
-//
-//    }
-//    return 10
-//}
+func fibi(k:Int) -> Int
+{
+    var fibArray :[Int] = [0, 1]
+    var i = 1
+    
+    while fibArray.count != k
+    {
+        fibArray.append(fibArray[i] + fibArray[i - 1])
+        i += 1
+    }
+    fibArray.append(fibArray[i] + fibArray[i - 1])
+    
+    return fibArray[k]
+}
 
 
-let result = fib(k: 10)
+let result = fibi(k: 10)
+let result2 = fib(k: 10)
 print(result)
+print(result2)
