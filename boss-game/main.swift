d9a0d3991652295b6e624309f40fa294c11d4adc
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2022-11-15 09:22:49 -0500

    Wrote a function that uses recursion to compute fib

---
+++ boss-game/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  boss-game
+//
+//  Created by Vincent He on 2022-11-15.
+//
+
+import Foundation
+
+func fib(k:Int) -> Int {
+    if k == 1 {
+        return 1
+    }
+    
+    return fib(k: k-1) + fib(k: k-2)
+}
+
+//func fibi(k:Int) -> Int{
+//    
+//    return 10
+//}
+
+
+let result = fib(k: 10)
+print(result)
