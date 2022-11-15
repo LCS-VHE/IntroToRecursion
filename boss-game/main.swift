766b99b6c26e0bb38ea2d7ee5df1d565c70881bd
Mr-Anyone <53135664+Mr-Anyone@users.noreply.github.com>
2022-11-15 09:43:43 -0500

    fib now works with edge cases; k = 1, k=0

--- boss-game/main.swift
+++ boss-game/main.swift
@@ -26,14 +26,14 @@ func fibi(k:Int) -> Int
 {
     var fibArray :[Int] = [0, 1]
     var i = 1
-    
-    while fibArray.count != k
+        
+    while fibArray.count <= k
     {
         fibArray.append(fibArray[i] + fibArray[i - 1])
         i += 1
     }
     fibArray.append(fibArray[i] + fibArray[i - 1])
-    
+
     return fibArray[k]
 }
 
